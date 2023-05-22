d3fb5cb011ec8cbbf91ee5fe393f6ecdc9ca8b3a
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-05-22 22:45:55 +0300

    add query solutions for questions #1 - #3

--- Case Study #4 : Data Bank/a_customer_nodes_exploration.sql
+++ Case Study #4 : Data Bank/a_customer_nodes_exploration.sql
@@ -11,14 +11,23 @@ for each region?
 -- A. Customer Nodes Exploration
 -- 1. How many unique nodes are there on the Data Bank system?
 
+select count(distinct region_id, node_id) as `Total number of unique nodes in the Data Bank System` from customer_nodes;
 
 -- 2. What is the number of nodes per region?
 
-
+select region_name as Region, 
+       count(distinct node_id) as `Number of nodes` 
+from customer_nodes 
+join regions using (region_id) 
+group by region_name;
 
 -- 3. How many customers are allocated to each region?
 
-
+select region_name as Region, 
+       count(distinct customer_id) as `Number of customers allocated` 
+from customer_nodes 
+join regions using (region_id) 
+group by region_name;
 
 -- 4. How many days on average are customers reallocated to a different node?
 
