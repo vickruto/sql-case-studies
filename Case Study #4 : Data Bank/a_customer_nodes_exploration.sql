e3c2691685a233cb6c68969aedcd29c71e00353c
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-05-23 00:22:13 +0300

    add query solution for question #5

--- Case Study #4 : Data Bank/a_customer_nodes_exploration.sql
+++ Case Study #4 : Data Bank/a_customer_nodes_exploration.sql
@@ -54,22 +54,60 @@ select round(avg(timestampdiff(day, start_d, end_d)),1) as `Avg no. of days befo
 from t3 
 where end_d <> '9999-12-31';
 
--- previous solutions ::
-with node_allocations_table as ( 
-	select *, 
-	   case when end_date='9999-12-31' then 0 else timestampdiff(day, start_date, end_date) end as days,
-	   case when customer_id = lag(customer_id) over()
-		and region_id = lag(region_id) over() 
-		and node_id = lag(node_id) over()
-	   then 0 else 1 end as allocated_different_node
-	from customer_nodes
-	order by customer_id, start_date)
-
-select round(sum(days)/(sum(allocated_different_node)-count(distinct customer_id)),1) as `Average number of days before reallocation to a different node`
-from node_allocations_table
-;
-
 -- 5. What is the median, 80th and 95th percentile for this same reallocation days metric for each region?
 
+set @a := 0;
+with t1 as (
+	select *, case when timestampdiff(day, lag(end_date) over(partition by customer_id,node_id), start_date)=1 then 0 else 1 end as is_different_node
+	from customer_nodes
+	order by customer_id, start_date),
+     
+     t2 as (
+	select *, (@a := @a +is_different_node) as row_i
+	from t1),
+	
+     t3 as (
+	select customer_id, 
+	       region_id,
+	       min(start_date) as start_d,
+	       max(end_date) as end_d,
+	       timestampdiff(day, min(start_date), max(end_date)) as allocation_days 
+	from t2
+	where end_date <> '9999-12-31'
+	group by customer_id, row_i, region_id),
 
+     t4 as (
+	select allocation_days, 
+	       region_id,
+	       region_count,
+	       rank() over(partition by region_id order by allocation_days, start_d, customer_id) as rnk,
+	       percent_rank() over(partition by region_id order by allocation_days, start_d, customer_id) as pct_rnk
+	from t3
+	join (select region_id, count(*) as region_count from t3 group by region_id) as x using (region_id)),
+
+     medians_table as (
+	select region_id, round(avg(allocation_days),1) as median_days
+	from t4 
+	where rnk in (floor((region_count+1)/2), 
+		      ceil((region_count+1)/2))
+	group by region_id),
+
+     50th_perc_table as (
+        select region_id, max(allocation_days) as 50th from t4 where pct_rnk <= 0.5 group by region_id),
+  
+     80th_perc_table as (
+        select region_id, max(allocation_days) as 80th from t4 where pct_rnk <= 0.8 group by region_id),
+        
+     95th_perc_table as (
+        select region_id, max(allocation_days) as 95th from t4 where pct_rnk <= 0.95 group by region_id)
+
+select region_name as `region`,
+       median_days as `median of allocation days`,
+       80th as `80th percentile`,
+       95th as `95th percentile`
+from medians_table
+join 80th_perc_table using (region_id)
+join 95th_perc_table using (region_id)
+join regions using (region_id)
+order by region_name;
 
