cd842263abeb83f60e0d72af7a7f63ce94d9c298
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-05-25 23:20:27 +0300

    add query solution for question #3

--- Case Study #5 : Data Mart/c_before_and_after_analysis.sql
+++ Case Study #5 : Data Mart/c_before_and_after_analysis.sql
@@ -46,5 +46,39 @@ join (
 
 -- 3. How do the sale metrics for these 2 periods before and after compare with the previous years in 2018 and 2019?
 
+-- a) Sale metrics for 4 weeks before and after the baseline week_date for all the years
+select calendar_year as Year,
+       4_week_before_sales as `Sales Before`,
+       4_week_after_sales as `Sales After`,
+       concat(round((4_week_after_sales/4_week_before_sales*100 - 100),2), '%') as `Percentage Change`
+from (
+	select calendar_year, sum(sales) as 4_week_before_sales
+	from clean_weekly_sales
+	where ceil(dayofyear(@baseline_week_date)/7)-week_number between 1 and 4
+	group by calendar_year) as b
+join (
+	select calendar_year, sum(sales) as 4_week_after_sales
+	from clean_weekly_sales
+	where ceil(dayofyear(@baseline_week_date)/7)-week_number between -3 and 0
+	group by calendar_year) as a
+using (calendar_year)
+;
 
+-- b) Sale metrics for 12 weeks before and after the baseline week_date for all the years
+select calendar_year as Year,
+       12_week_before_sales as `Sales Before`,
+       12_week_after_sales as `Sales After`,
+       concat(round((12_week_after_sales/12_week_before_sales*100 - 100),2), '%') as `Percentage Change`
+from (
+	select calendar_year, sum(sales) as 12_week_before_sales
+	from clean_weekly_sales
+	where ceil(dayofyear(@baseline_week_date)/7)-week_number between 1 and 12
+	group by calendar_year) as b
+join (
+	select calendar_year, sum(sales) as 12_week_after_sales
+	from clean_weekly_sales
+	where ceil(dayofyear(@baseline_week_date)/7)-week_number between -11 and 0
+	group by calendar_year) as a
+using (calendar_year)
+;
 
