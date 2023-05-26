8944e03952ddd5ec070a2bb4b69736885c99a64d
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-05-26 22:55:58 +0300

    add query solutions for questions #3 - #5

--- Case Study #6 : Clique Bait/b_digital_analysis.sql
+++ Case Study #6 : Clique Bait/b_digital_analysis.sql
@@ -22,3 +22,28 @@ select count(distinct user_id) from users;
 
 select count(*)/count(distinct user_id) as `average number of cookies per user` from users ;
 
+-- 3. What is the unique number of visits by all users per month?
+
+select monthname(start_date) as month, 
+       count(distinct cookie_id) as `number of visits` 
+from users 
+group by monthname(start_date), month(start_date)
+order by month(start_date);
+
+-- 4. What is the number of events for each event type?
+
+select event_type, event_name, count(*) as `event counts` 
+from events 
+join event_identifier using (event_type) 
+group by event_name, event_type;
+
+-- 5. What is the percentage of visits which have a purchase event?
+
+select concat(round(num_purchase_events/total_unique_events*100, 2), '%') as `percentage of visits leading to purchase`
+from (
+	select count(distinct visit_id) as num_purchase_events
+	from events 
+	join event_identifier using (event_type) 
+	where event_name like '%Purchase%') a
+join (  select count(distinct visit_id) as total_unique_events from events) b;
+
