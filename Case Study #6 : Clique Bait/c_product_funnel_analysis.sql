99b5e08ec65286d2ea78ba6e4a056c8c85008a63
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-05-27 01:58:46 +0300

    add product category statistics table query

--- Case Study #6 : Clique Bait/c_product_funnel_analysis.sql
+++ Case Study #6 : Clique Bait/c_product_funnel_analysis.sql
@@ -62,6 +62,45 @@ join purchases_ using (product)
 join abandonments_ using (product);
 
 
+-- Additionally, create another table which further aggregates the data for the above points but this time for each product category instead of individual products.
+
+drop table if exists product_categories_statistics_table;
+create table product_categories_statistics_table as 
+with joined_tables as (
+	select visit_id, sequence_number, product_category, event_name 
+	from events 
+	join page_hierarchy using (page_id) 
+	join event_identifier using (event_type)),
+
+     num_views_ as (
+	select product_category, count(*) as views
+	from joined_tables 
+	where event_name like '%Page View%'
+	group by product_category), 
+
+     cart_adds_ as (
+	select product_category, count(*) as cart_adds
+	from joined_tables 
+	where event_name like '%Cart%'
+	group by product_category),
+
+     purchases_ as (
+	select product_category, count(*) as purchases
+	from joined_tables 
+	where event_name like '%Cart%' and visit_id in (select visit_id from joined_tables where event_name like '%Purchase%')
+	group by product_category),
+
+     abandonments_ as (
+	select product_category, count(*) as abandonments
+	from joined_tables 
+	where event_name like '%Cart%' and visit_id not in (select visit_id from joined_tables where event_name like '%Purchase%')
+	group by product_category)
+
+select *
+from num_views_ 
+join cart_adds_ using (product_category)
+join purchases_ using (product_category)
+join abandonments_ using (product_category);
 
 -- Use your 2 new output tables - answer the following questions:
 -- 1. Which product had the most views, cart adds and purchases?
