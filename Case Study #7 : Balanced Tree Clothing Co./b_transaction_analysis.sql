f87a91272978990e834a0f84d541cacfe660b33d
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-05-27 23:15:22 +0300

    transaction analysis initial commit

---
+++ Case Study #7 : Balanced Tree Clothing Co./b_transaction_analysis.sql
@@ -0,0 +1,35 @@
+/*
+Transaction Analysis
+1. How many unique transactions were there?
+2. What is the average unique products purchased in each transaction?
+3. What are the 25th, 50th and 75th percentile values for the revenue per transaction?
+4. What is the average discount value per transaction?
+5. What is the percentage split of all transactions for members vs non-members?
+6. What is the average revenue for member transactions and non-member transactions?
+*/
+
+
+-- 1. How many unique transactions were there?
+
+
+
+-- 2. What is the average unique products purchased in each transaction?
+
+
+
+-- 3. What are the 25th, 50th and 75th percentile values for the revenue per transaction?
+
+
+
+-- 4. What is the average discount value per transaction?
+
+
+
+-- 5. What is the percentage split of all transactions for members vs non-members?
+
+
+
+-- 6. What is the average revenue for member transactions and non-member transactions?
+
+
+
