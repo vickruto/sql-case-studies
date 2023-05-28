6a71736d380d1ffb68cc76613fc14cefefb6b983
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-05-28 21:44:58 +0300

    add product analysis with query solutions for questions #1 - #2

---
+++ Case Study #7 : Balanced Tree Clothing Co./c_product_analysis.sql
@@ -0,0 +1,69 @@
+/*
+Product Analysis
+1. What are the top 3 products by total revenue before discount?
+2. What is the total quantity, revenue and discount for each segment?
+3. What is the top selling product for each segment?
+4. What is the total quantity, revenue and discount for each category?
+5. What is the top selling product for each category?
+6. What is the percentage split of revenue by product for each segment?
+7. What is the percentage split of revenue by segment for each category?
+8. What is the percentage split of total revenue by category?
+9. What is the total transaction “penetration” for each product? (hint: penetration = number of transactions where at least 1 quantity of a product was purchased divided by total number of transactions)
+10. What is the most common combination of at least 1 quantity of any 3 products in a single transaction?
+*/
+
+-- Product Analysis
+-- 1. What are the top 3 products by total revenue before discount?
+
+select pd.product_name, 
+       concat('$', format(sum(s.qty*s.price), 2)) as `total revenue before discount` 
+from sales s 
+join product_details pd 
+on s.prod_id=pd.product_id 
+group by product_name;
+
+--- 2. What is the total quantity, revenue and discount for each segment?
+
+select pd.segment_name as `segment`, 
+       sum(qty) as `total quantity`,
+       concat('$', format(sum(s.qty*s.price),2)) as `gross revenue`, 
+       concat('$', format(sum(s.qty*s.price*discount/100),2)) as `discount`
+from product_details pd 
+join sales s on pd.product_id=s.prod_id 
+group by pd.segment_name ;
+limit 4;
+
+
+-- 3. What is the top selling product for each segment?
+
+
+
+-- 4. What is the total quantity, revenue and discount for each category?
+
+
+
+-- 5. What is the top selling product for each category?
+
+
+
+-- 6. What is the percentage split of revenue by product for each segment?
+
+
+
+-- 7. What is the percentage split of revenue by segment for each category?
+
+
+
+-- 8. What is the percentage split of total revenue by category?
+
+
+
+-- 9. What is the total transaction “penetration” for each product? (hint: penetration = number of transactions where at least 1 quantity of a product was purchased divided by total number of transactions)
+
+
+
+-- 10. What is the most common combination of at least 1 quantity of any 3 products in a single transaction?
+
+
+
+
