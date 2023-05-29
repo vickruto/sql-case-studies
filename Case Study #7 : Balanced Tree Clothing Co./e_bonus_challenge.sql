04bba73694e19b36d438153f22decd955f519fa5
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-05-29 23:53:58 +0300

    bonus challenge initial commit

---
+++ Case Study #7 : Balanced Tree Clothing Co./e_bonus_challenge.sql
@@ -0,0 +1,5 @@
+/*
+Bonus Challenge
+Use a single SQL query to transform the product_hierarchy and product_prices datasets to the product_details table.
+Hint: you may want to consider using a recursive CTE to solve this problem!
+*/
