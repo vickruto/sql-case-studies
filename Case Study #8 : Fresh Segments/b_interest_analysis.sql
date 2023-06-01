b51d5d4ee52a9e132eeea667f2408e556e6e2718
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-06-01 23:46:46 +0300

    interest analysis initial commit with query solution for question #1

---
+++ Case Study #8 : Fresh Segments/b_interest_analysis.sql
@@ -0,0 +1,48 @@
+/*
+Interest Analysis
+1. Which interests have been present in all `month_year` dates in our dataset?
+2. Using this same `total_months` measure - calculate the cumulative percentage of all records starting at 14 months - which `total_months` value passes the 90% cumulative percentage value?
+3. If we were to remove all `interest_id `values which are lower than the `total_months` value we found in the previous question - how many total data points would we be removing?
+4. Does this decision make sense to remove these data points from a business perspective? Use an example where there are all 14 months present to a removed `interest` example for your arguments - think about what it means to have less months present from a segment perspective.
+5. After removing these interests - how many unique interests are there for each month?
+*/
+
+use fresh_segments; 
+
+-- Interest Analysis
+-- 1. Which interests have been present in all `month_year` dates in our dataset?
+
+with x (total_months) as (
+	select count(distinct month_year) from interest_metrics), 
+	
+     monthly_counts as (
+	 select interest_id, count(distinct month_year) as num_months from interest_metrics group by interest_id order by count(distinct month_year)),
+
+     interests as (
+          select interest_id, num_months from monthly_counts join x where num_months=total_months)
+
+select interest_name as "Some interests present in all `month_year` dates" 
+from interests i 
+join interest_map mp on i.interest_id=mp.id  
+limit 10;
+
+/*********************************
+  There are 480 interests that have been present in all `month_year` dates in the dataset 
+*********************************/
+
+-- 2. Using this same `total_months` measure - calculate the cumulative percentage of all records starting at 14 months - which `total_months` value passes the 90% cumulative percentage value?
+
+
+
+-- 3. If we were to remove all `interest_id `values which are lower than the `total_months` value we found in the previous question - how many total data points would we be removing?
+
+
+
+-- 4. Does this decision make sense to remove these data points from a business perspective? Use an example where there are all 14 months present to a removed `interest` example for your arguments - think about what it means to have less months present from a segment perspective.
+
+
+
+-- 5. After removing these interests - how many unique interests are there for each month?
+
+
+
