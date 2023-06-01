d83cf9acfec843da35fece2e4f70b3a1d423ac91
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-06-01 23:56:23 +0300

    add query solutions for questions #2, #3

--- Case Study #8 : Fresh Segments/b_interest_analysis.sql
+++ Case Study #8 : Fresh Segments/b_interest_analysis.sql
@@ -32,10 +32,34 @@ limit 10;
 
 -- 2. Using this same `total_months` measure - calculate the cumulative percentage of all records starting at 14 months - which `total_months` value passes the 90% cumulative percentage value?
 
+with interest_total_months as (
+	select interest_id, count(distinct month_year) as total_months 
+	from interest_metrics 
+	where interest_id is not null 
+	group by interest_id order by total_months desc), 
+
+      x as (select *, round(percent_rank() over(order by total_months desc)*100, 2) as cumulative_percentage from interest_total_months)
+      
+select distinct total_months, cumulative_percentage from x;
+
+/*********************************
+ All total_months values above 6 months pass the 90% cumulative percentage value. 90.92% of all records involve interests which are present in more than 6 months 
+*********************************/
 
 
 -- 3. If we were to remove all `interest_id `values which are lower than the `total_months` value we found in the previous question - how many total data points would we be removing?
 
+select count(*) `Number of records with interests occuring in less than 6 months`
+from interest_metrics 
+join (select interest_id, 
+      count(distinct month_year) as num_months 
+      from interest_metrics group by interest_id) as x 
+using (interest_id) 
+where num_months <= 6 ;
+
+/*********************************
+ There are 598 records from 143 interests which occur for less than 6 months of the 14 months in the dataset 
+*********************************/
 
 
 -- 4. Does this decision make sense to remove these data points from a business perspective? Use an example where there are all 14 months present to a removed `interest` example for your arguments - think about what it means to have less months present from a segment perspective.
