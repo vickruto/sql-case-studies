7b398f05763715226636184c881c13006a1f1f43
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-06-02 12:46:44 +0300

    add recursive query that generates `product_details` table from `product_hierarchy` and `product_prices` tables

--- Case Study #7 : Balanced Tree Clothing Co./e_bonus_challenge.sql
+++ Case Study #7 : Balanced Tree Clothing Co./e_bonus_challenge.sql
@@ -22,3 +22,41 @@ join product_hierarchy ph_seg on ph_stl.parent_id = ph_seg.id
 join product_hierarchy ph_cat on ph_seg.parent_id = ph_cat.id
 order by style_id;
 
+
+-- RECURSIVE SOLUTION
+with recursive cte as (
+        select id,  
+               id as category_id,
+               cast(level_text as nchar(20)) as category_name,
+               0 as segment_id, 
+               cast(null as nchar(20)) as segment_name,
+               0 as style_id, 
+               cast(null as nchar(20)) as style_name, 
+               1 as lvl
+        from product_hierarchy 
+        where parent_id is null
+        union all
+        select ph.id,
+               cte.category_id,
+               cte.category_name,
+               case when ph.level_name='Segment' then ph.id else cte.segment_id end as segment_id,
+               case when ph.level_name='Segment' then ph.level_text else cte.segment_name end as segment_name,
+               case when ph.level_name='Style' then ph.id else cte.style_id end as style_id,
+               case when ph.level_name='Style' then ph.level_text else cte.style_name end as style_name,
+               lvl + 1 as lvl
+        from cte
+        join product_hierarchy ph
+        on cte.id = ph.parent_id 
+        where lvl < 4
+        )
+select product_id,
+       price,
+       concat(style_name, ' ', segment_name, ' - ', category_name) as product_name,
+       category_id,
+       segment_id,
+       style_id,
+       category_name,
+       segment_name,
+       style_name
+from cte
+join product_prices using (id);
