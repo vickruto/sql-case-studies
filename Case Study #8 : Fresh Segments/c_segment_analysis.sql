3de63efbb225044bc11f0fb47dc2a073d9877464
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-06-02 10:28:02 +0300

    add query solutions for question #1 (top & bottom 10 interests by composition)

--- Case Study #8 : Fresh Segments/c_segment_analysis.sql
+++ Case Study #8 : Fresh Segments/c_segment_analysis.sql
@@ -12,7 +12,51 @@ use fresh_segments;
 -- Segment Analysis
 -- 1. Using our filtered dataset by removing the interests with less than 6 months worth of data, which are the top 10 and bottom 10 interests which have the largest composition values in any month_year ? Only use the maximum composition value for each interest but you must keep the corresponding month_year
 
+-- TOP 10 INTERESTS BY COMPOSITION
+with filtered_interest_metrics as (
+	select im.*
+	from interest_metrics im
+	join (select interest_id, 
+	      count(distinct month_year) as num_months 
+	      from interest_metrics group by interest_id) as x 
+	using (interest_id) 
+	where num_months >= 6 and month_year is not null),
 
+      max_composition_filtered_interest_metrics as (
+	select * from (select *, rank() over(partition by interest_id order by composition desc) as rnk from filtered_interest_metrics) as x where rnk=1), 
+      
+      top_10_compositions_table as (
+	select interest_id, month_year, interest_name, composition
+	from max_composition_filtered_interest_metrics c
+	join interest_map m 
+	on m.id = c.interest_id
+	order by composition desc
+	limit 10)
+
+select * from top_10_compositions_table; 
+
+-- BOTTOM 10 INTERESTS BY COMPOSITION
+with filtered_interest_metrics as (
+	select im.*
+	from interest_metrics im
+	join (select interest_id, 
+	      count(distinct month_year) as num_months 
+	      from interest_metrics group by interest_id) as x 
+	using (interest_id) 
+	where num_months >= 6 and month_year is not null),
+
+      min_composition_filtered_interest_metrics as (
+	select * from (select *, rank() over(partition by interest_id order by composition asc) as rnk from filtered_interest_metrics) as x where rnk=1), 
+      
+      bottom_10_compositions_table as (
+	select interest_id, month_year, interest_name, composition
+	from min_composition_filtered_interest_metrics c
+	join interest_map m 
+	on m.id = c.interest_id
+	order by composition asc
+	limit 10)
+
+select * from bottom_10_compositions_table; 
 
 -- 2. Which 5 interests had the lowest average ranking value?
 
