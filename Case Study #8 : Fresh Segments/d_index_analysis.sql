9f42b75fa9092a00fad0191dd59ba4c193aee084
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-06-02 20:05:50 +0300

    add query solution for question #3

--- Case Study #8 : Fresh Segments/d_index_analysis.sql
+++ Case Study #8 : Fresh Segments/d_index_analysis.sql
@@ -73,7 +73,27 @@ Three interests appear in the monthly top 10 interests in 10 of the 14 months in
 
 -- 3. What is the average of the average composition for the top 10 interests for each month?
 
+with avg_comp_table as (
+	select *, 
+	       round(composition/index_value,2) as avg_composition, 
+	       rank() over(partition by month_year order by composition/index_value desc) as rnk
+	from interest_metrics mt
+	join interest_map mp
+	on mt.interest_id = mp.id
+	where month_year is not null),
 
+     top_10_avg_comp_by_month_table as (
+	select interest_id, 
+	       month_year,
+	       round(composition/index_value,2) as avg_composition
+	from avg_comp_table 
+	where rnk <= 10
+	order by month_year, rnk)
+
+select month_year, 
+       round(avg(avg_composition), 2) as `average of the average composition`
+from top_10_avg_comp_by_month_table
+group by month_year;
 
 -- 4. What is the 3 month rolling average of the max average composition value from September 2018 to August 2019 and include the previous top ranking interests in the same output shown below.
 
