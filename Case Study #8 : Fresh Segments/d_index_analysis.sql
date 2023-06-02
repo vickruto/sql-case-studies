889fc0213391a922555dcc7463d69008ae570305
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-06-02 22:03:53 +0300

    add query solution for question #4

--- Case Study #8 : Fresh Segments/d_index_analysis.sql
+++ Case Study #8 : Fresh Segments/d_index_analysis.sql
@@ -97,7 +97,35 @@ group by month_year;
 
 -- 4. What is the 3 month rolling average of the max average composition value from September 2018 to August 2019 and include the previous top ranking interests in the same output shown below.
 
+with avg_comp_table as (
+	select *, 
+	       round(composition/index_value,2) as avg_composition, 
+	       rank() over(partition by month_year order by composition/index_value desc) as rnk
+	from interest_metrics mt
+	join interest_map mp
+	on mt.interest_id = mp.id
+	where month_year is not null),
 
+    top_avg_comp_by_month as (
+	select month_year,
+	       interest_name,
+	       round(composition/index_value,2) as avg_composition
+	from avg_comp_table 
+	where rnk <= 1),
+
+    solution_table as (
+	select month_year,
+	       interest_name,
+	       avg_composition as max_index_composition,
+	       round(avg(avg_composition) over(order by month_year rows between 2 preceding and 0 following),2) as _3_month_moving_avg,
+	       concat(lag(interest_name, 1, null) over w, ': ', lag(avg_composition, 1, null) over w) as _1_month_ago,
+	       concat(lag(interest_name, 2, null) over w, ': ', lag(avg_composition, 2, null) over w) as _2_month_ago
+	from top_avg_comp_by_month
+	window w as (order by month_year))
+
+select * 
+from solution_table 
+where month_year >= '2018-09-01';
 
 --- 5. Provide a possible reason why the max average composition might change from month to month? Could it signal something is not quite right with the overall business model for Fresh Segments?
 
