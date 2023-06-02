5f3942100fae77288e192049dcbd2d62bdd32807
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-06-02 17:46:25 +0300

    error fix : add corrected query solutions for questions #1, #2

--- Case Study #8 : Fresh Segments/d_index_analysis.sql
+++ Case Study #8 : Fresh Segments/d_index_analysis.sql
@@ -13,42 +13,63 @@ use fresh_segments;
 
 -- 1. What is the top 10 interests by the average composition for each month?
 
+with avg_comp_table as (
+	select *, 
+	       round(composition/index_value,2) as avg_composition, 
+	       rank() over(partition by month_year order by composition/index_value desc) as rnk
+	from interest_metrics mt
+	join interest_map mp
+	on mt.interest_id = mp.id
+	where month_year is not null)
+
 select interest_id, 
        month_year,
        interest_name,
        index_value,
        ranking,
        percentile_ranking,
-       round(composition/index_value,2) as avg_composition 
-from interest_metrics mt
-join interest_map mp
-on mt.interest_id = mp.id
-order by avg_composition desc 
-limit 10;
+       round(composition/index_value,2) as avg_composition
+from avg_comp_table 
+where rnk <= 10
+order by month_year, rnk;
 
 -- 2. For all of these top 10 interests - which interest appears the most often?
 
-with top_interests_by_avg_comp as (
+with avg_comp_table as (
+	select *, 
+	       round(composition/index_value,2) as avg_composition, 
+	       rank() over(partition by month_year order by composition/index_value desc) as rnk
+	from interest_metrics mt
+	join interest_map mp
+	on mt.interest_id = mp.id
+	where month_year is not null),
+
+     top_10_avg_comp_by_month_table as (
 	select interest_id, 
 	       month_year,
 	       interest_name,
 	       index_value,
 	       ranking,
 	       percentile_ranking,
-	       round(composition/index_value,2) as avg_composition 
-	from interest_metrics mt
-	join interest_map mp
-	on mt.interest_id = mp.id
-	order by avg_composition desc 
-	limit 10)
+	       round(composition/index_value,2) as avg_composition
+	from avg_comp_table 
+	where rnk <= 10
+	order by month_year, rnk)
 
-select interest_name as `interest with most occurence in top 10`, 
+select interest_name as `Interests with most occurence in top 10`, 
        interest_id,
        count(*) as `Number of occurences`
-from top_interests_by_avg_comp 
+from top_10_avg_comp_by_month_table 
 group by interest_name, interest_id
 order by count(*) desc
-limit 1;
+limit 10;
+
+/***************************************************************************
+Three interests appear in the monthly top 10 interests in 10 of the 14 months in the dataset. These interests are :
+     - Solar Energy Researchers
+     - Alabama Trip Planners 
+     - Luxury Bedding Shoppers
+****************************************************************************/
 
 -- 3. What is the average of the average composition for the top 10 interests for each month?
 
