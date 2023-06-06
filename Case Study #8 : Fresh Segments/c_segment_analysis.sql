98368dc19c58692e09b628f28c70d79079532278
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-06-06 17:37:36 +0300

    create a view of filtered interest_metrics table where interests with less than 6 months worth of data are removed

--- Case Study #8 : Fresh Segments/c_segment_analysis.sql
+++ Case Study #8 : Fresh Segments/c_segment_analysis.sql
@@ -12,17 +12,19 @@ use fresh_segments;
 -- Segment Analysis
 -- 1. Using our filtered dataset by removing the interests with less than 6 months worth of data, which are the top 10 and bottom 10 interests which have the largest composition values in any month_year ? Only use the maximum composition value for each interest but you must keep the corresponding month_year
 
--- TOP 10 INTERESTS BY COMPOSITION
-with filtered_interest_metrics as (
+create view filtered_interest_metrics 
+as 
 	select im.*
 	from interest_metrics im
 	join (select interest_id, 
 	      count(distinct month_year) as num_months 
 	      from interest_metrics group by interest_id) as x 
 	using (interest_id) 
-	where num_months >= 6 and month_year is not null),
+	where num_months >= 6 and month_year is not null
+;
 
-      max_composition_filtered_interest_metrics as (
+-- TOP 10 INTERESTS BY COMPOSITION
+with max_composition_filtered_interest_metrics as (
 	select * from (select *, rank() over(partition by interest_id order by composition desc) as rnk from filtered_interest_metrics) as x where rnk=1), 
       
       top_10_compositions_table as (
@@ -36,16 +38,7 @@ with filtered_interest_metrics as (
 select * from top_10_compositions_table; 
 
 -- BOTTOM 10 INTERESTS BY COMPOSITION
-with filtered_interest_metrics as (
-	select im.*
-	from interest_metrics im
-	join (select interest_id, 
-	      count(distinct month_year) as num_months 
-	      from interest_metrics group by interest_id) as x 
-	using (interest_id) 
-	where num_months >= 6 and month_year is not null),
-
-      min_composition_filtered_interest_metrics as (
+with min_composition_filtered_interest_metrics as (
 	select * from (select *, rank() over(partition by interest_id order by composition asc) as rnk from filtered_interest_metrics) as x where rnk=1), 
       
       bottom_10_compositions_table as (
@@ -59,15 +52,6 @@ with filtered_interest_metrics as (
 select * from bottom_10_compositions_table; 
 
 -- 2. Which 5 interests had the lowest average ranking value?
-
-with filtered_interest_metrics as (
-	select im.*
-	from interest_metrics im
-	join (select interest_id, 
-	      count(distinct month_year) as num_months 
-	      from interest_metrics group by interest_id) as x 
-	using (interest_id) 
-	where num_months >= 6 and month_year is not null)
 	
 select interest_name, round(avg(ranking),2) as avg_ranking
 from filtered_interest_metrics f 
@@ -78,15 +62,6 @@ limit 5;
 
 -- 3. Which 5 interests had the largest standard deviation in their percentile_ranking value?
 
-with filtered_interest_metrics as (
-	select im.*
-	from interest_metrics im
-	join (select interest_id, 
-	      count(distinct month_year) as num_months 
-	      from interest_metrics group by interest_id) as x 
-	using (interest_id) 
-	where num_months >= 6 and month_year is not null)
-
 select distinct interest_id, interest_name , stddev(percentile_ranking) over(partition by interest_id) as std_deviation 
 from filtered_interest_metrics f 
 join interest_map m on m.id = f.interest_id  
@@ -95,16 +70,7 @@ limit 5;
 
 -- 4. For the 5 interests found in the previous question - what was minimum and maximum percentile_ranking values for each interest and its corresponding year_month value? Can you describe what is happening for these 5 interests?
 
-with filtered_interest_metrics as (
-	select im.*
-	from interest_metrics im
-	join (select interest_id, 
-	      count(distinct month_year) as num_months 
-	      from interest_metrics group by interest_id) as x 
-	using (interest_id) 
-	where num_months >= 6 and month_year is not null),
-
-     largest_std_dev_table as (
+with largest_std_dev_table as (
 	select distinct interest_id, interest_name , stddev(percentile_ranking) over(partition by interest_id) as std_deviation 
 	from filtered_interest_metrics f 
 	join interest_map m on m.id = f.interest_id  
